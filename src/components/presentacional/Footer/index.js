66381a4ab3c79bed929c3003e0cc92c5223da6d2
carloscastanedan <carloscastanedan@gmail.com>
2019-12-20 15:17:57 -0600

    updated: footer v1

--- src/components/presentacional/Footer/index.js
+++ src/components/presentacional/Footer/index.js
@@ -3,7 +3,56 @@ import React from 'react'
 
 const Footer = () => (
   <footer>
-    Footer
+    <div className='first-section'>
+      <div>
+        <a href='/#'>Iniciar Sesión</a>
+        <hr />
+        <div className='info-container'>
+          <div className='section'>
+            <h6>Soporte al cliente</h6>
+            <a href='/#'>Facturación</a>
+            <a href='/#'>Ayuda</a>
+            <a href='/#'>Mis Pedidos</a>
+          </div>
+          <div className='section'>
+            <h6>Soporte al cliente</h6>
+            <a href='/#'>Facturación</a>
+            <a href='/#'>Ayuda</a>
+            <a href='/#'>Mis Pedidos</a>
+          </div>
+          <div className='section'>
+            <h6>Soporte al cliente</h6>
+            <a href='/#'>Facturación</a>
+            <a href='/#'>Ayuda</a>
+            <a href='/#'>Mis Pedidos</a>
+          </div>
+          <div className='section'>
+            <h6>Soporte al cliente</h6>
+            <a href='/#'>Facturación</a>
+            <a href='/#'>Ayuda</a>
+            <a href='/#'>Mis Pedidos</a>
+          </div>
+        </div>
+        <hr />
+        <div className='social'>
+          <div>Comentarios y Sugerencias:atencion@liverpool.com.mx</div>
+          <div>
+            <a href='/#'>Facturación</a>
+            <a href='/#'>Facturación</a>
+            <a href='/#'>Facturación</a>
+          </div>
+          <div className='social-icons'>
+            <div>Facebook</div>
+            <div>Twiter</div>
+            <div>Instagram</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className='second-section'>
+      <div>recio en rojo aplica el descuento más alto, las promociones pueden diferir de las publicadas en tienda</div>
+      <div>2019 Liverpool / Todos los derechos reservados D.R. ®</div>
+    </div>
   </footer>
 )
 
