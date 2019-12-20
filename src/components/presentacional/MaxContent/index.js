e6cb41f89fb692cd40f8f78bb12ae573df1f613d
Carlos Castañeda <carloscastaneda@MacBook-Pro-de-Carlos.local>
2019-12-20 13:21:05 -0600

    updated: header footer, logo was added

---
+++ src/components/presentacional/MaxContent/index.js
@@ -0,0 +1,13 @@
+// Import Modules
+import React from 'react'
+
+// Import styles
+import './styles.scss'
+
+const MaxContent = ({ children }) => (
+  <div className='max-content'>
+    {children}
+  </div>
+)
+
+export default MaxContent
